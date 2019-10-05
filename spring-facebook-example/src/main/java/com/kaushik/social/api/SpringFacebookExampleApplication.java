7b6880606cf981d48a2f02bbd6992defa1865d7e
krvaghani1 <kaushik.vaghani111@gmail.com>
2019-10-05 16:32:33 +0530

    first commit

---
+++ spring-facebook-example/src/main/java/com/kaushik/social/api/SpringFacebookExampleApplication.java
@@ -0,0 +1,13 @@
+package com.kaushik.social.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFacebookExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringFacebookExampleApplication.class, args);
+	}
+
+}
